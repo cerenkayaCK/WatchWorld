184409c869e118df0d65adb84aeba41ed3fef6eb
Ceren Kaya <cerenkaya1997ck@hotmail.com>
2024-02-28 09:44:53 +0300

    entities

---
+++ src/ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; set; }
+    }
+}
